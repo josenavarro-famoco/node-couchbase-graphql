8dae5334b207f4908d49640f19f250d770c8a8a9
= <jmn8718@gmail.com>
2017-02-10 13:57:50 +0100

    added spartial query

--- src/db/index.js
+++ src/db/index.js
@@ -22,7 +22,13 @@ let myBucket = myCluster.openBucket(bucket, function(err) {
 let ottoman = require('ottoman');
 ottoman.store = new ottoman.CbStoreAdapter(myBucket, couchbase);
 
+const queryByLocation = (bbox = [0, 0, 0, 0], next) => {
+  const query = couchbase.SpatialQuery.from('dev_place_by_location', 'place_by_location').bbox(bbox);
+  myBucket.query(query, next);
+}
+
 module.exports = {
   bucket: myBucket,
-  ottoman: ottoman
+  ottoman: ottoman,
+  queryByLocation: queryByLocation
 };
